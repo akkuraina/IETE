ffdb85d1cc1fe4e863be0f838e5d9a2fd3d1229b
akanksha raina <akanksharainadjsce@gmail.com>
2024-08-11 14:51:28 +0530

    Add files via upload
    this is a task given by IETE-SF for joining their co-comm. took about 10 hours total, still continuing to update it.

---
+++ iete.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    const ctaButton = document.querySelector('.cta-button');
+
+    ctaButton.addEventListener('mouseover', function() {
+        ctaButton.style.transform = 'scale(1.1)';
+    });
+
+    ctaButton.addEventListener('mouseout', function() {
+        ctaButton.style.transform = 'scale(1)';
+    });
+};
+
